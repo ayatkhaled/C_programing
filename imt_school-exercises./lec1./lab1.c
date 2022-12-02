47289d3c6075bad38d9f179626571c99249329b7
ayatkhaled <71589348+ayatkhaled@users.noreply.github.com>
2022-12-02 07:57:56 +0200

    Create lab1.c

---
+++ imt_school-exercises./lec1./lab1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+printf("MY name is Ayat\n");
+
+printf("my birthday is 16/5/1999\n");
+printf("I graduated from fuculty of engineering suez canal university 2022\n");
+
+}
